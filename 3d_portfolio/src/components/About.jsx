f72a74a6c0f1d965a6c14c4e161ee7c117ab2135
linaMallek <92185257+linaMallek@users.noreply.github.com>
2024-12-20 22:00:12 +0100

    add about

--- 3d_portfolio/src/components/About.jsx
+++ 3d_portfolio/src/components/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from 'react-tilt' ;
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-import { SectionWrapper } from "../hoc";
+import {SectionWrapper} from "../hoc";
+
+
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
